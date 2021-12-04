6b270ac998f2013e2c0ccfb46d4cf76594021441
kimseonghwan <harrykim0731@naver.com>
2021-12-04 15:54:06 +0900

    resolving print change

--- re_solving.py
+++ re_solving.py
@@ -1,7 +1,7 @@
 answer = 5
 problems=['sin', 'cos', 'acrtan', 'sec', 'css']
 pick = 0
-print('다음중 삼각함수가 아닌 함수를 고르시요',problems)
+print('다음중 삼각함수가 아닌 함수를 고르시요1.',problems)
 while pick != answer:
     pick = int(input('1부터 5중에 정답이라고 생각하는 답안 입력'))
     if pick == answer:
