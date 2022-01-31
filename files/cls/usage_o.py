4a617f3f397f890ce11f968dc6b0e2f8abad7c12
김성환 <harrykim0731@gimjaehuns-MacBook-Pro.local>
2022-01-31 14:42:08 +0900

    student_grades_upgrade

--- files/cls/usage_o.py
+++ files/cls/usage_o.py
@@ -2,24 +2,25 @@ class testsum:
     def __init__(self):
         '이 class는 시험 점수의 합과 평균을 계산해 줍니다.'
 
-    def file_write(self):
-        line = ['100\n', '90\n', '89\n', '88\n', '100\n']
-        with open('../student_grades.txt', 'r')as file:
-            file.writelines(line)
-
     def file_read(self):  # 파일의 값을 배열로 리턴하는 함수
-        h = []
-        with open('../cls/student_grades.txt', 'r')as file:
+        mt = []
+        ko = []
+        with open('./student_grades.txt', 'r') as file:
             line = None
             while line != '':
                 line = file.readline()
                 a = line.strip('\n')
                 if (a != ''):
-                    h.append(int(a))
-        return h
+                    tmp = a.split(',')
+                mt.append(tmp[0])
+                ko.append(tmp[1])
+        return mt, ko
 
-    def file_print(self, list):  # 파일의 값들의 합과 편균 구하는 함수
+    def file_print(self, ko, mt):  # 파일의 값들의 합과 편균 구하는 함수
         x = 0
-        for i in list:
-            x = x + i
-        print('시험 점수의 총합', x, '시험 점수의 평균', x / len(list))
+        y = 0
+        for i in ko:
+            x = x + int(i)
+        for j in mt:
+            y = y + int(j)
+        print('국어 점수의 총합', x, '국어 점수의 평균', x / len(ko), '수학 점수의 총합', y, '수학 점수의 평균', y / len(mt))
--- files/useage_of_file.py
+++ files/useage_of_file.py
@@ -3,5 +3,5 @@ import files.cls.usage_o as use
 myClass = use.testsum()
 
 # myClass.file_write()
-i = myClass.file_read()
-myClass.file_print(i)
+i= myClass.file_read()
+myClass.file_print(i[0],i[1])
